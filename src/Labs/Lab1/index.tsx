1df3b612cb91d081404df3370e03f3c3ba94dc2c
Misha Tabatabai <mishat2021@gmail.com>
2025-01-12 14:16:59 -0500

    introducing headings

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,3 +1,8 @@
 export default function Lab1() {
-    return <h2>Lab 1</h2>;
+    return (<div>
+        <h2>Lab 1</h2>
+        <h3>Headings</h3>
+        <p>Headings allow you to breakup the content into multiple sections</p>
+        </div>
+        );
 }
