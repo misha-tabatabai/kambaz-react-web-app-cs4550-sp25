82b99b19b03bc245eeb3ecaf9811fd4ea4579793
Misha Tabatabai <mishat2021@gmail.com>
2025-04-10 16:59:37 -0400

    fingers crossed push

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -4,7 +4,6 @@ import { useSelector } from "react-redux";
 export default function AccountNavigation() {
   const { pathname } = useLocation();
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const active = (path: string) => (pathname.includes(path) ? "active" : "");
 
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
