92f827759157bd6c501fd5aa9c2c10babccaac05
Misha Tabatabai <mishat2021@gmail.com>
2025-04-12 14:09:18 -0400

    everything works until 6.4.3.1

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -15,6 +15,33 @@ export default function Modules() {
   const { cid } = useParams();
   const { modules } = useSelector((state: any) => state.modulesReducer);
   const dispatch = useDispatch();
+  const updateModuleHandler = async (module: any) => {
+    await modulesClient.updateModule(module);
+    dispatch(updateModule(module));
+  };
+
+  const deleteModuleHandler = async (moduleId: string) => {
+    await modulesClient.deleteModule(moduleId);
+    dispatch(deleteModule(moduleId));
+  };
+
+  const addModuleHandler = async () => {
+    const newModule = await coursesClient.createModuleForCourse(cid!, {
+      name: moduleName,
+      course: cid,
+    });
+    dispatch(addModule(newModule));
+    setModuleName("");
+  };
+
+  const fetchModulesForCourse = async () => {
+    const modules = await coursesClient.findModulesForCourse(cid!);
+    dispatch(setModules(modules));
+  };
+  useEffect(() => {
+    fetchModulesForCourse();
+  }, [cid]);
+
   const saveModule = async (module: any) => {
     await modulesClient.updateModule(module);
     dispatch(updateModule(module));
@@ -42,7 +69,7 @@ export default function Modules() {
       <ModulesControls
         moduleName={moduleName}
         setModuleName={setModuleName}
-        addModule={createModuleForCourse}
+        addModule={addModuleHandler}
       />
       <br /><br /><br /><br />
       <ListGroup id="wd-modules" className="rounded-0">
@@ -60,11 +87,10 @@ export default function Modules() {
                     <FormControl
                       className="w-50 d-inline-block"
                       onChange={(e) =>
-                        dispatch(updateModule({ ...module, name: e.target.value }))
-                      }
+                        updateModuleHandler({ ...module, name: e.target.value })}
                       onKeyDown={(e) => {
                         if (e.key === "Enter") {
-                          saveModule({ ...module, editing: false });
+                          updateModuleHandler({ ...module, editing: false });
                         }
                       }}
                       defaultValue={module.name}
@@ -72,7 +98,7 @@ export default function Modules() {
                   )}
                   <ModuleControlButtons
                     moduleId={module._id}
-                    deleteModule={(moduleId) => removeModule(moduleId)}
+                    deleteModule={(moduleId) => deleteModuleHandler(moduleId)}
                     editModule={(moduleId) => dispatch(editModule(moduleId))}
                   />
                 </div>
--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -8,7 +8,6 @@ import "./styles.css";
 import { useEffect, useState } from "react";
 import { ProtectedRoute } from "./Account/ProtectedRoute";
 import Session from "./Account/Session";
-import * as userClient from "./Account/client";
 import * as courseClient from "./Courses/client";
 import { useSelector, useDispatch } from "react-redux";
 import { setEnrolledCourses } from "./Enrollments/reducer";
@@ -20,7 +19,7 @@ export default function Kambaz() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const fetchCourses = async () => {
     try {
-      const courses = await userClient.findMyCourses();
+      const courses = await courseClient.fetchAllCourses();
       setCourses(courses);
       if (currentUser?.role === "STUDENT") {
         dispatch(setEnrolledCourses(courses));
@@ -49,10 +48,11 @@ export default function Kambaz() {
     startDate: "2023-09-10", endDate: "2023-12-15", description: "New Description",
   });
   const addNewCourse = async () => {
-    const newCourse = await userClient.createCourse(course);
+    const newCourse = await courseClient.createCourse(course);
     setCourses([...courses, newCourse]);
   };
   const deleteCourse = async (courseId: any) => {
+    const status = await courseClient.deleteCourse(courseId);
     setCourses(courses.filter((course) => course._id !== courseId));
   };
   const updateCourse = async () => {
